ac21405e7bbf9c945f3c795083f86bf45e99f3ce
akemioshiro@hotmail.com <akemioshiro@hotmail.com>
2016-10-24 22:07:21 -0200

    CrudComAngularJsWebApi

---
+++ CrudComAngularJsWebApi/Models/Celular.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudComAngularJsWebApi.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Marca { get; set; }
+
+        public string Modelo { get; set; }
+
+        public string Cor { get; set; }
+
+        public string TipoChip { get; set; }
+
+        public string MemoriaInterna { get; set; }
+
+    }
+}
